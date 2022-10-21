afcfb7c52719e3e5c49bfe393dabc6b1f6c05def
Victor Chigozie Nwafor <ubfacility@gmail.com>
2022-10-21 15:45:44 +0100

    fix: mysql connection to dockerized instance.

--- database/connect.go
+++ database/connect.go
@@ -1,9 +1,6 @@
 package database
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/Rajil1213/go-admin/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -12,13 +9,14 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	db_username := os.Getenv("DB_USERNAME")
-	db_host := os.Getenv("DB_HOST")
-	db_password := os.Getenv("DB_PASSWORD")
-	db_database := os.Getenv("DB_DATABASE")
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", db_username, db_password, db_host, db_database)
+	// db_username := os.Getenv("DB_USERNAME")
+	// db_host := os.Getenv("DB_HOST")
+	// db_password := os.Getenv("DB_PASSWORD")
+	// db_database := os.Getenv("DB_DATABASE")
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", db_username, db_password, db_host, db_database)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/fullstack_api"), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to the database")
